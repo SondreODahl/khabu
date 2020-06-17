a0eb611131654619bc6cb6d7f749efec16547fdc
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-06-17 14:14:50 +0200

    Basic frontend setup
    Removed start src/folder
    Created folders for front and backend
    Created basic index.js file for displaying something

---
+++ front-end/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+    return (
+        <div>React App</div>
+    );
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector("#root")
+)
