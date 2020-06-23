20544c5910f4744df1ce4b43621d4d6e294a3a36
SondreODahl <sondreo.dahl@gmail.com>
2020-06-23 14:43:07 +0200

    attempt at fixing cors issue

--- back-end/src/main/java/com/khabu/cardgame/controllers/HelloController.java
+++ back-end/src/main/java/com/khabu/cardgame/controllers/HelloController.java
@@ -1,11 +1,12 @@
 package com.khabu.cardgame.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
-    @GetMapping("/api/hello")
+
+    @CrossOrigin
+    @RequestMapping(value="/api/hello", method = RequestMethod.GET, produces = "application/json")
     public String hello() {
         return "Hei på deg T, logg inn på Oracle her :)";
     }
--- front-end/src/api/Server.js
+++ front-end/src/api/Server.js
@@ -1,5 +1,5 @@
 import Axios from "axios";
 
-export default axios.create({
-    baseURL: 'https://localhost/8080' // TODO: Add production URL
-}); 
+export default Axios.create({
+  baseURL: "http://localhost/8080", // TODO: Add production URL
+});
