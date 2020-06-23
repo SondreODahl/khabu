5217e4b6aa43ec5cfeef76ddf703ca9148321663
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-06-23 19:02:05 +0200

    Fixed CORS issue

--- back-end/src/main/java/com/khabu/cardgame/controllers/HelloController.java
+++ back-end/src/main/java/com/khabu/cardgame/controllers/HelloController.java
@@ -1,13 +1,26 @@
 package com.khabu.cardgame.controllers;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+// @CrossOrigin(origins="http://localhost:3000") // To have it class-wide
 @RestController
 public class HelloController {
 
-    @CrossOrigin
     @RequestMapping(value="/api/hello", method = RequestMethod.GET, produces = "application/json")
     public String hello() {
         return "Hei på deg T, logg inn på Oracle her :)";
+    }
+
+    @Bean // For global CORS
+    public WebMvcConfigurer configureMvc () {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*").allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 }
--- front-end/src/api/Server.js
+++ front-end/src/api/Server.js
@@ -1,5 +1,5 @@
 import Axios from "axios";
 
 export default Axios.create({
-  baseURL: "http://localhost/8080", // TODO: Add production URL
+  baseURL: "http://localhost:8080", // TODO: Add production URL
 });
