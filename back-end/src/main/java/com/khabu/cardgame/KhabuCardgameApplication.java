6af5df7a43b4605f56d73bc6e8a8064ba015ee3a
SondreODahl <sondreo.dahl@gmail.com>
2020-06-23 13:13:07 +0200

    add files from spring initializr

---
+++ back-end/src/main/java/com/khabu/cardgame/KhabuCardgameApplication.java
@@ -0,0 +1,13 @@
+package com.khabu.cardgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhabuCardgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhabuCardgameApplication.class, args);
+	}
+
+}
