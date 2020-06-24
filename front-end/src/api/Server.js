0bf7269763cd420fdc1087e4b178d2b9d8ffcefb
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-06-24 13:10:20 +0200

    Fixed API_URL

--- front-end/src/api/Server.js
+++ front-end/src/api/Server.js
@@ -2,5 +2,5 @@ import Axios from 'axios';
 import { config } from './Constants';
 
 export default Axios.create({
-  baseURL: config.url, // TODO: Add production URL
+  baseURL: config.url.API_URL, // TODO: Add production URL
 });
