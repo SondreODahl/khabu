44046368ba6ecf1cca277a289dfceff3b8c46bfd
SondreODahl <sondreo.dahl@gmail.com>
2020-06-25 10:40:54 +0200

    add basic websocket functionality

--- back-end/src/main/java/com/khabu/cardgame/controllers/HelloController.java
+++ back-end/src/main/java/com/khabu/cardgame/controllers/HelloController.java
@@ -1,6 +1,8 @@
 package com.khabu.cardgame.controllers;
 
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -8,9 +10,21 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class HelloController {
 
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    public HelloController(SimpMessagingTemplate simpMessagingTemplate) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+    }
+
     @RequestMapping(value="/api/hello", method = RequestMethod.GET, produces = "application/json")
     public String hello() {
         return "Hei på deg T, logg inn på Oracle her :)";
+    }
+
+    @MessageMapping("/greeting")
+    public void greet(String greeting) {
+        String text = "Hello" + greeting;
+        this.simpMessagingTemplate.convertAndSend("/greeting", text);
     }
 
 }
