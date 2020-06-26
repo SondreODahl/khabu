fa4c2a25e022c97fd69c2911d1130c9965c22587
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-06-26 16:13:37 +0200

    Build hotfix 🔥

--- front-end/src/api/STOMPClient.js
+++ front-end/src/api/STOMPClient.js
@@ -4,7 +4,6 @@ import usePublish from './usePublish';
 import useClient from './useClient';
 import GreetButtons from '../components/GreetButton';
 import GreatButton from '../components/GreatButton';
-import UserGreetButton from '../components/UserGreetButton';
 import ReadyUpButton from '../components/ReadyUpButton';
 
 const STOMPClient = (props) => {
