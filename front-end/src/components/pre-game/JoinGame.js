798880bf13ba5258000714ebb4e270f79e384e5c
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-07-01 13:56:05 +0200

    Add multiple error messages

--- front-end/src/components/pre-game/JoinGame.js
+++ front-end/src/components/pre-game/JoinGame.js
@@ -4,7 +4,6 @@ import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 import { joinSubmit } from '../../actions';
 import { useRESTPost } from '../../api/RESTServer';
-import { ErrorMessage } from '@hookform/error-message';
 
 export default (props) => {
   const { register, errors, handleSubmit } = useForm({ criteriaMode: 'all' });
@@ -27,18 +26,33 @@ export default (props) => {
           name={'userName'}
           type={'text'}
           ref={register({
-            required: true,
-            minLength: 5,
-            maxLength: 20,
-            pattern: /^[A-Za-z0-9]+$/,
+            required: { value: true, message: 'You must input a username' },
+            minLength: {
+              value: 5,
+              message: 'Username must be minimum 5 characters',
+            },
+            maxLength: {
+              value: 20,
+              message: 'Username must be maximum 20 characters',
+            },
+            pattern: {
+              value: /^[A-Za-z0-9]+$/,
+              message: 'Username must be only letters/numbers',
+            },
           })}
         />
-        {errors?.userName?.type === 'pattern' &&
-          'Username must be only letters and numbers'}
-        {errors?.userName?.type === 'maxLength' &&
-          'Username must be maximum 20 characters'}
-        {errors?.userName?.type === 'minLength' &&
-          'Username must be minimum 5 characters'}
+        {errors?.userName && errors?.userName?.types?.pattern && (
+          <div>Username must be only letters and numbers</div>
+        )}
+        {errors?.userName && errors?.userName?.types?.maxLength && (
+          <div>Username must be maximum 20 characters</div>
+        )}
+        {errors?.userName && errors?.userName?.types?.minLength && (
+          <div>Username must be minimum 5 characters</div>
+        )}
+        {errors?.userName && errors?.userName?.types?.required && (
+          <div>Username must be minimum 5 characters</div>
+        )}
       </form>
     </div>
   );
