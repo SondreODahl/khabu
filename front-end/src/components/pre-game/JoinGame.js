869017679fe405a06a31d0b3e8a44603d95f5967
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-07-01 12:32:37 +0200

    Basic form in JoinGame

--- front-end/src/components/pre-game/JoinGame.js
+++ front-end/src/components/pre-game/JoinGame.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 export default (props) => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div>
-      <div>Enter Username:</div>
       <Link to={'/'}>
         <button>Home</button>
       </Link>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        Enter username:
+        <input name={'userName'} type={'text'} ref={register} />
+      </form>
     </div>
   );
 };
