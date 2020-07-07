90cdd82d597aaa1e45fefb6c28bbdd32b89e3119
SondreODahl <sondreo.dahl@gmail.com>
2020-07-07 12:45:23 +0200

    Add classes: Player.java and Turn.java. Add enum: Gamestate.java

---
+++ back-end/src/main/java/com/khabu/cardgame/model/game/Gamestate.java
@@ -0,0 +1,10 @@
+package com.khabu.cardgame.model.game;
+
+public enum Gamestate {
+    DRAW,
+    PUT,
+    KHABU,
+    FRENZY,
+    FIRST_TURN,
+    START
+}
--- back-end/src/main/java/com/khabu/cardgame/model/game/Player.java
+++ back-end/src/main/java/com/khabu/cardgame/model/game/Player.java
@@ -1,6 +1,33 @@
 package com.khabu.cardgame.model.game;
 
 public class Player {
+    private String name;
+    private int playerId;
+    private CardHand cardHand;
+    private boolean ready = false;
 
-    public Player(String name, int Id) {}
+    public Player(String name, int playerId) {
+        this.name = name;
+        this.playerId = playerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPlayerId() {
+        return playerId;
+    }
+
+    public CardHand getCardHand() {
+        return cardHand;
+    }
+
+    public boolean isReady() {
+        return ready;
+    }
+
+    void setReady(boolean ready) {
+        this.ready = ready;
+    }
 }
