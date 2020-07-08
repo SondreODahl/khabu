8e6306bd8bc14db3b51a81835df233f613d90ee6
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-07-08 13:55:21 +0200

    Extend IllegalMove from IllegalState

--- back-end/src/main/java/com/khabu/cardgame/util/IllegalMoveException.java
+++ back-end/src/main/java/com/khabu/cardgame/util/IllegalMoveException.java
@@ -1,6 +1,6 @@
 package com.khabu.cardgame.util;
 
-public class IllegalMoveException extends Exception {
+public class IllegalMoveException extends IllegalStateException {
     public IllegalMoveException() { super(); }
     public IllegalMoveException(String message) { super(message); }
     public IllegalMoveException(String message, Throwable cause) { super(message, cause); }
