902c72090fa2d21e3f8c87d4f7648a450562428d
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-07-08 16:23:33 +0200

    Add nextPlayer functionality and tests to turn.

--- back-end/src/main/java/com/khabu/cardgame/model/game/Turn.java
+++ back-end/src/main/java/com/khabu/cardgame/model/game/Turn.java
@@ -6,16 +6,29 @@ import com.khabu.cardgame.util.IllegalMoveException;
 import com.khabu.cardgame.util.NotYourTurnException;
 
 public class Turn {
-    private Player currentPlayer;
     private Player currentPuttingPlayer;
+    private Player[] players;
+    private int currentPlayer;
     private Gamestate gameState;
 
-    public void endTurn() throws NotYourTurnException, IllegalMoveException {
-
+    public Turn(Player[] players) {
+        this.players = players;
+        currentPlayer = 0;
     }
 
-    public void updateCurrentPlayer(Player newCurrentPlayer) {
-        this.currentPlayer = newCurrentPlayer;
+    public void nextPlayer() {
+        currentPlayer = (currentPlayer + 1) % players.length;
+        currentPuttingPlayer = null;
+    }
+
+    public void setCurrentPlayer(Player newCurrentPlayer) {
+        for (int i = 0; i < players.length; i++) {
+            if (players[i] == newCurrentPlayer) {
+                this.currentPlayer = i;
+                return;
+            }
+        }
+        throw new IllegalArgumentException("Player not in list of players");
     }
 
     public void updateCurrentPuttingPlayer(Player puttingPlayer) {
@@ -23,7 +36,7 @@ public class Turn {
     }
 
     public Player getCurrentPlayer() {
-        return currentPlayer;
+        return players[currentPlayer];
     }
 
     public Player getCurrentPuttingPlayer() {
--- back-end/src/test/java/com/khabu/cardgame/model/game/TurnTest.java
+++ back-end/src/test/java/com/khabu/cardgame/model/game/TurnTest.java
@@ -1,12 +1,52 @@
 package com.khabu.cardgame.model.game;
 
-import com.khabu.cardgame.model.game.action.Actions;
-import com.khabu.cardgame.model.game.action.Gamestate;
-import com.khabu.cardgame.util.IllegalMoveException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TurnTest {
 
+    private Turn turn;
+
+    Player[] players = {new Player("player1", 1), new Player("player", 2)};
+
+    @BeforeEach
+    void setUp() {
+        turn = new Turn(players);
+    }
+
+    @Test
+    void testNextTurnLoop() {
+        turn.setCurrentPlayer(players[0]);
+        turn.nextPlayer();
+        Player currentPlayer = turn.getCurrentPlayer();
+        assertEquals(players[1], currentPlayer);
+        turn.nextPlayer();
+        currentPlayer = turn.getCurrentPlayer();
+        assertEquals(players[0], currentPlayer);
+    }
+
+    @Test
+    void testSetPlayerToOneNotInGame() {
+        try {
+            turn.setCurrentPlayer(new Player("rwrwe", 2));
+            fail();
+        }
+        catch (IllegalArgumentException ignored) {}
+    }
+
+    @Test
+    void testGetCurrentPlayer() {
+        turn.setCurrentPlayer(players[1]);
+        assertEquals(players[1], turn.getCurrentPlayer());
+    }
+
+    @Test
+    void testCurrentPuttingPlayerResetOnNextTurn() {
+        turn.updateCurrentPuttingPlayer(players[0]);
+        assertEquals(players[0], turn.getCurrentPuttingPlayer());
+        turn.nextPlayer();
+        assertNull(turn.getCurrentPuttingPlayer());
+    }
 }
