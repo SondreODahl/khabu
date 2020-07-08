2e60aa38cd957ef5232a8cb6f919a857ff16d07c
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-07-08 16:24:03 +0200

    Add skeleton methods to Round

--- back-end/src/main/java/com/khabu/cardgame/model/game/Round.java
+++ back-end/src/main/java/com/khabu/cardgame/model/game/Round.java
@@ -1,7 +1,30 @@
 package com.khabu.cardgame.model.game;
 
+import com.khabu.cardgame.model.game.action.Actions;
+
 public class Round {
 
     public Round(int numOfPlayers) {}
 
+    public void beginRound() {
+        // This is the logic before first player's turn
+        // TODO: Deal cards
+        // TODO: Reveal cards
+        // TODO: Set a starting player
+    }
+
+    public boolean getStarted() {
+        return true;
+    }
+
+    public void performAction(Player player2, Actions discard) {
+    }
+
+    public boolean readyUp(Player player1) {
+        return false;
+    }
+
+    public int getPlayersReady() {
+        return 0;
+    }
 }
