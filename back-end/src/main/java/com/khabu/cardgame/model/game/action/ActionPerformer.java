db52bd44a12d6616c95f0ad1f3808ee6a7fad21e
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-07-09 20:25:35 +0200

    Add methods for put and endturn in Performer

--- back-end/src/main/java/com/khabu/cardgame/model/game/action/ActionPerformer.java
+++ back-end/src/main/java/com/khabu/cardgame/model/game/action/ActionPerformer.java
@@ -1,18 +1,55 @@
 package com.khabu.cardgame.model.game.action;
 
 import com.khabu.cardgame.model.game.*;
+import com.khabu.cardgame.util.IllegalMoveException;
 
 public class ActionPerformer {
 
-    private Turn turn;
+    private final Turn turn;
+    private final CardDeck cardDeck;
+    private final DiscardPile discardPile;
+    private Card temporaryCard;
 
     public ActionPerformer(Turn turn, CardDeck cardDeck, DiscardPile discardPile) {
+        if (turn == null || cardDeck == null || discardPile == null)
+            throw new IllegalArgumentException("Argument cannot be null");
+        this.turn = turn;
+        this.cardDeck = cardDeck;
+        this.discardPile = discardPile;
     }
 
-    public void putSelf(Player player, int i) {
+    private void validateAction(Player player, Actions action, Turn turn) {
+        if (!ActionValidator.isValidMoveInCurrentState(player, action, turn)) {
+            throw new IllegalMoveException("IllegalMove, tried to %s")
+        }
+    }
+
+    private Card putExecutor(Player player1, Player player2, int index, Actions action) throws IllegalArgumentException, IllegalMoveException {
+        validateAction(player1, action, turn);
+        Card card = player2.getCard(index);
+        if (discardPile.showTopCard().isSameValue(card)) {
+            discardPile.put(card);
+            return player2.removeCard(index);
+        }
+        return null;
+    }
+
+    public void putSelf(Player player, int index) throws IllegalArgumentException, IllegalMoveException{
+        putExecutor(player, player, index, Actions.PUT_SELF);
+        turn.setGameState(Gamestate.PUT);
+        turn.setCurrentPuttingPlayer(player);
     }
 
     public void endTurn(Player player) {
+        validateAction(player, Actions.END_TURN, turn);
+        turn.nextPlayer();
+        Gamestate nextState;
+        if (turn.getCurrentPlayer() == turn.getKhabuPlayer()) {
+            nextState = Gamestate.ENDED;
+            Round.endRound(); // TODO: Replace
+        } else
+            nextState = Gamestate.DRAW;
+        turn.setGameState(nextState);
     }
 
     public Card drawFromDeck(Player player) {
@@ -33,9 +70,11 @@ public class ActionPerformer {
     public void swapDrawnCard(Player player, int index) {
     }
 
-    public void discardCard(Player player) {}
+    public void discardCard(Player player) {
+    }
 
-    public void transferCard(Player player1, Player player2, int cardIndex) {}
+    public void transferCard(Player player1, Player player2, int cardIndex) {
+    }
 
     /*
     public endTurn(Player player) {
