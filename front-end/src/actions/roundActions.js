4e495491bbb03679d510018d005bde0850c18db1
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-07-15 20:17:06 +0200

    Add roundActions and state for reducers

---
+++ front-end/src/actions/roundActions.js
@@ -0,0 +1,5 @@
+import { ALL_PLAYERS_READY } from './types';
+
+export const initializeRound = () => {
+  return { type: ALL_PLAYERS_READY };
+};
--- front-end/src/reducers/index.js
+++ front-end/src/reducers/index.js
@@ -4,12 +4,16 @@ import { clientReducer, connectedClientReducer } from './API/clientReducer';
 import subReducer from './API/subReducer';
 import joinFormReducer from './joinFormReducer';
 import readyReducer from './readyReducer';
+import roundReducer from './roundReducer';
+import gameReducer from './gameReducer';
 
 export default combineReducers({
   data: restReducer,
   client: clientReducer,
   connected: connectedClientReducer,
+  game: gameReducer,
   subscription: subReducer,
+  round: roundReducer,
   ready: readyReducer,
   form: joinFormReducer,
 });
