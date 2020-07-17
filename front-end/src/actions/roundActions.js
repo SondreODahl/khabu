ea664c24e479cf5d890a60b8ae2e8c3229b9f473
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-07-17 11:15:00 +0200

    Add thunk to initializeRound action creator
    Done so that you can access state within it

--- front-end/src/actions/roundActions.js
+++ front-end/src/actions/roundActions.js
@@ -1,7 +1,8 @@
 import { ALL_PLAYERS_READY, START_ROUND } from './types';
 
-export const initializeRound = () => {
-  return { type: ALL_PLAYERS_READY };
+export const initializeRound = () => (dispatch, getState) => {
+  const playerIds = getState().players.allPlayers;
+  dispatch({ type: ALL_PLAYERS_READY, payload: { playerIds } });
 };
 
 export const startRound = () => {
