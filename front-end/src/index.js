241c07e58e058933213a6d19dcf2629a3ca8959e
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-07-17 11:01:20 +0200

    Add redux-thunk ➕

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { compose, createStore } from 'redux';
+import { applyMiddleware } from 'redux';
 
 import App from './components/App';
 import reducers from './reducers';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Testing from './components/testing/Testing';
+import thunk from 'redux-thunk';
 
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
