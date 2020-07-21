76044fceefb3d4cee2f0dd5fce5efaf351049762
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-07-21 12:26:21 +0200

    Add gamestates and actions for affecting them

--- front-end/src/actions/types.js
+++ front-end/src/actions/types.js
@@ -41,3 +41,11 @@ export const END_TURN = 'END_TURN';
 
 // ------------------ GameState --------------------------
 export const PLAYER_CALLED_KHABU = 'PLAYER_CALLED_KHABU';
+export const DRAW_FROM_DECK = 'DRAW_FROM_DECK';
+export const SUCCESSFUL_PUT_SELF = 'SUCCESSFUL_PUT_SELF';
+export const SUCCESSFUL_PUT_OTHER = 'SUCCESSFUL_PUT_OTHER';
+export const DRAW_FROM_DECK = 'DRAW_FROM_DECK';
+export const TRANSFER_CARD = 'TRANSFER_CARD';
+export const SWAP_CARDS = 'SWAP_CARDS';
+export const SWAP_WITH_DISC = 'SWAP_WITH_DISC';
+export const DISCARD_CARD = 'DISCARD_CARD';
---
+++ front-end/src/reducers/game/gameStates.js
@@ -0,0 +1,8 @@
+export const DRAW = 'DRAW';
+export const FIRST_TURN = 'FIRST_TURN';
+export const CARD_DRAWN = 'CARD_DRAWN';
+export const PUT_OTHER_TRANSFER = 'PUT_OTHER_TRANSFER';
+export const PUT = 'PUT';
+export const DISCARD = 'DISCARD';
+export const FRENZY = 'FRENZY';
+export const TRANSFER = 'TRANSFER';
