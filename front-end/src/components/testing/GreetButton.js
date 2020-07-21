10cbad3fa61c1954b2d740b3adaccf8eb97935dc
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-07-21 10:42:36 +0200

    Add JSON test for GreetButton

--- front-end/src/components/testing/GreetButton.js
+++ front-end/src/components/testing/GreetButton.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import usePublish from '../../api/usePublish';
 import useSubscribe from '../../api/useSubscribe';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { addSubMessage } from '../../actions';
+import { retrievePlayers } from '../../actions/playerActions';
 
 export default (props) => {
   useSubscribe('/topic/greeting', addSubMessage);
@@ -13,10 +14,23 @@ export default (props) => {
     body: 'Greet',
   });
 
+  const dispatch = useDispatch();
+  const test_json = {
+    type: 'PLAYER_INFO',
+    yourId: 1,
+    playerIds: { 1: 'Humlesnurr', 2: 'Whackamole' },
+  };
+
   return (
     <div>
       <button className={'ui teal button'} onClick={greetPublish}>
         Publish
+      </button>
+      <button
+        className={'ui blue button'}
+        onClick={() => dispatch(retrievePlayers(test_json))}
+      >
+        JSON
       </button>
       {message}
     </div>
