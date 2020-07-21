be65630b28155e21457886946eb41be8f14b41b9
SondreODahl <sondreo.dahl@gmail.com>
2020-07-21 10:55:35 +0200

    Create the gameController class

---
+++ back-end/src/main/java/com/khabu/cardgame/controllers/GameController.java
@@ -0,0 +1,8 @@
+package com.khabu.cardgame.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GameController {
+    // TODO: implement /round/actions/playerId with responses to drawing
+}
