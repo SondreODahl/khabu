d59b015838e1b0ab68a977f33fc685165d420449
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-07-27 10:05:16 +0200

    Add card selector tests to testing page

--- front-end/src/components/testing/Testing.js
+++ front-end/src/components/testing/Testing.js
@@ -1,18 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import GreetButton from './GreetButton';
 import ReadyUpButton from '../in-game/ReadyUpButton';
 import { useRESTGet } from '../../api/RESTServer';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { initializeRound, startRound } from '../../actions';
+import { retrievePlayers } from '../../actions/playerActions';
+import { createSelector } from 'reselect';
+import { getCardIndexForCard, selectCard, selectCardHand } from '../../selectors';
+import { revealCard } from '../../actions/cardActions';
+import HandIndexSelector from './HandIndexSelector';
 
 export default (props) => {
   const url = '/api/hello';
   const { getRESTData } = useRESTGet();
   const data = useSelector((state) => state.api.get_data);
+  const dispatch = useDispatch();
+  const IdInCardHand = useSelector((state) => selectCard(state, { id: 0 }));
+  console.log(IdInCardHand);
+  const cardHand = useSelector((state) => selectCardHand(state, { playerId: 0 }));
+  console.log(cardHand);
 
   useEffect(() => {
     getRESTData(url);
   }, [getRESTData]);
+
+  useEffect(() => {
+    dispatch(
+      retrievePlayers({
+        yourId: '0',
+        playerIds: { '0': 'Humlesnurr' },
+      })
+    );
+    dispatch(initializeRound(1, 4));
+    dispatch(startRound(0));
+    dispatch(revealCard(0, 1337));
+  }, []);
+
+  const render = () => {
+    if (cardHand) {
+      return <HandIndexSelector />;
+    }
+  };
 
   return (
     <div>
@@ -24,6 +53,7 @@ export default (props) => {
         <div className={'column'}>
           <GreetButton />
           {data}
+          {render()}
         </div>
         <div className={'column'}>
           <ReadyUpButton />
