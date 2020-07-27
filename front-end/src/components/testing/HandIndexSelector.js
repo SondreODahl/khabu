514086b076b06c3fcd7db3f9f43fb997ba69106d
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-07-27 12:17:01 +0200

    Refactor getCardIndex to getServerId

--- front-end/src/components/testing/HandIndexSelector.js
+++ front-end/src/components/testing/HandIndexSelector.js
@@ -1,10 +1,10 @@
 import { useSelector } from 'react-redux';
-import { getCardIndexForCard } from '../../selectors';
+import { getServerIdForCard } from '../../selectors';
 import React from 'react';
 
 export default () => {
   const indexOfCard2 = useSelector((state) =>
-    getCardIndexForCard(state, { playerId: 0, id: 0 })
+    getServerIdForCard(state, { playerId: 0, id: 0 })
   );
   console.log(`Index ${indexOfCard2}`);
   return <div>{indexOfCard2}</div>;
