3e3afb50d4325e81811ee6276bb27e374d0eb32c
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-07-28 13:48:24 +0200

    Create basic DiscardPile

--- front-end/src/components/in-game/cards/DiscardPile.js
+++ front-end/src/components/in-game/cards/DiscardPile.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import Card from './Card';
-import { getDiscardPileTopCard } from '../../../selectors';
+import { getDiscardPileTopCard, selectDiscardPileLength } from '../../../selectors';
+import { useSelector } from 'react-redux';
+import { getDiscardPileAction } from '../../../selectors/gameStateSelectors';
+import CardDisplay from './CardDisplay';
+import DiscardPileImage from '../../../assets/images/discardpile_empty.png';
 
 export default () => {
+  const possibleAction = useSelector(getDiscardPileAction);
+  const length = useSelector(selectDiscardPileLength);
+  if (length === 0)
+    return (
+      <div>
+        <CardDisplay
+          onClick={null}
+          value={'Discard Pile'}
+          image={DiscardPileImage}
+        />
+        {possibleAction}
+      </div>
+    );
   return (
     <div className={'discard-pile'}>
       <Card selector={getDiscardPileTopCard} />
+      {possibleAction}
     </div>
   );
 };
