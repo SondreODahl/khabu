1d29259ec19f75d84b1e5099b7f33629b50f47c8
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-07-30 11:31:58 +0200

    Create selectPlayerNameById cached selector

--- front-end/src/selectors/playerSelectors.js
+++ front-end/src/selectors/playerSelectors.js
@@ -1,5 +1,7 @@
 import { createSelector } from 'reselect';
+import createCachedSelector from 're-reselect';
 
+const selectProps = (_, props) => props;
 export const selectYourId = (state) => state.players.yourId;
 export const selectAllPlayers = (state) => state.players.allPlayers;
 export const selectOpponentId = createSelector(
@@ -7,4 +9,8 @@ export const selectOpponentId = createSelector(
   selectAllPlayers,
   (yourId, allPlayers) => allPlayers.filter((ids) => ids !== yourId).pop()
 );
-export const selectPlayerName = (state, props) => state.players.byId[props.playerId];
+
+export const selectPlayerNameById = createCachedSelector(
+  (state, props) => state.players.byId[props.playerId],
+  (name) => name
+)((state, props) => props.playerId);
