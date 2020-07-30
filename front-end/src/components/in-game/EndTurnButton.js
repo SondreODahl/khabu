a4341437a9dcad93bc3217b56368a03df1534b38
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-07-30 10:46:40 +0200

    Add selector for determining when player can end turn

--- front-end/src/components/in-game/EndTurnButton.js
+++ front-end/src/components/in-game/EndTurnButton.js
@@ -1,13 +1,21 @@
 import usePublishMove from './cards/usePublishMove';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { getCanEndTurn } from '../../selectors/gameStateSelectors';
 
 export default (props) => {
   const publishEndTurn = usePublishMove('END_TURN', {
     currentPlayerId: props.yourId,
   });
+  const playerCanEndTurn = useSelector((state) => getCanEndTurn(state));
+  const onClick = playerCanEndTurn ? publishEndTurn : null;
+  const className = playerCanEndTurn ? 'active' : 'inactive';
+
   return (
     <div>
-      <button onClick={publishEndTurn}>End Turn</button>
+      <button onClick={onClick} className={`ui toggle button ${className}`}>
+        End Turn
+      </button>
     </div>
   );
 };
