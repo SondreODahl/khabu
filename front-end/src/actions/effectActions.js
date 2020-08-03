d95fc30846da3b84a21de3828bcb0331ce774063
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-08-03 15:17:23 +0200

    Add choose card move

--- front-end/src/actions/effectActions.js
+++ front-end/src/actions/effectActions.js
@@ -1,5 +1,9 @@
-import { ACTIVATE_EFFECT } from './types';
+import { ACTIVATE_EFFECT, CHOOSE_CARD_FOR_EFFECT } from './types';
 
 export const activateEffect = () => {
   return { type: ACTIVATE_EFFECT };
+};
+
+export const chooseCardForEffect = (cardId) => {
+  return { type: CHOOSE_CARD_FOR_EFFECT, payload: { cardId } };
 };
--- front-end/src/selectors/effectSelectors.js
+++ front-end/src/selectors/effectSelectors.js
@@ -2,6 +2,7 @@ import { DISCARD, USING_EFFECT } from '../constants/gameStates';
 import { createSelector } from 'reselect';
 import {
   CHECK_TWO_CARDS,
+  CHOOSE_CARD,
   EXCHANGE_CARDS,
   PLAYER_CHECK_OTHER,
   PLAYER_CHECK_SELF,
@@ -15,8 +16,7 @@ export const getEffectType = createSelector(selectEffectType, (type) => {
   if (type === null) return type;
   if (type < 9) return PLAYER_CHECK_SELF;
   if (type < 11) return PLAYER_CHECK_OTHER;
-  if (type < 13) return EXCHANGE_CARDS;
-  return type === 13 ? CHECK_TWO_CARDS : null;
+  return type <= 13 ? CHOOSE_CARD : null;
 });
 
 export const getIsUsingEffect = createSelector(
