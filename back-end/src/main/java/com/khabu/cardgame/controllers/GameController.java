02c29f830e62a48a1451554c55699983f5d98c31
Sondre Dahl <sondreo.dahl@gmail.com>
2020-08-04 21:06:22 +0200

    Move finish effect to GameEffectHandler

--- back-end/src/main/java/com/khabu/cardgame/controllers/GameController.java
+++ back-end/src/main/java/com/khabu/cardgame/controllers/GameController.java
@@ -322,22 +322,7 @@ public class GameController {
 
 
     private void finishEffect(HashMap<String, Object> jsonMap, Round round) {
-        int currentPlayerId = Integer.parseInt((String) jsonMap.get("currentPlayerId"));
-        if (round.getTurn().getCurrentPlayer() != round.getPlayerById(currentPlayerId)) {
-            sendError(Integer.toString(currentPlayerId));
-        }
-        if (jsonMap.get("swap").equals("true")) {
-            exchangeCardsAfterChecking(jsonMap, round);
-        }
-        if (round.getTurn().gameStateEquals(Gamestate.KING_EFFECT)) {
-            Gamestate gameState = round.getTurn().getCurrentPuttingPlayer() == null ? Gamestate.FRENZY : Gamestate.PUT;
-            round.getTurn().setGameState(gameState);
-        }
-
-        List<String> keys = Arrays.asList("type", "swap");
-        List<String> values = Arrays.asList("FINISH_EFFECT", jsonMap.get("swap").toString());
-
-        String jsonResponse = JsonConverter.createJsonString(new ObjectMapper(), new HashMap<>(), keys, values);
+        String jsonResponse = GameEffectHandler.handleFinishEffect(jsonMap, round);
         simpMessagingTemplate.convertAndSend("/topic/round/actions", jsonResponse);
     }
 
@@ -471,17 +456,7 @@ public class GameController {
     }
 
 
-    private void exchangeCardsAfterChecking(HashMap<String, Object> jsonMap, Round round) {
-        try {
-            int currentPlayerId = Integer.parseInt((String) jsonMap.get("currentPlayerId"));
-            round.performEffect(round.getPlayerById(currentPlayerId), 0, Effect.EXCHANGE_AFTER_CHECKS);
 
-            // Clear variables set to handle effect in effectPerformer
-            round.clearTemps();
-        } catch (IllegalMoveException ime) {
-            ime.printStackTrace();
-        }
-    }
 
 
 
--- back-end/src/main/java/com/khabu/cardgame/gameutil/GameEffectHandler.java
+++ back-end/src/main/java/com/khabu/cardgame/gameutil/GameEffectHandler.java
@@ -2,11 +2,14 @@ package com.khabu.cardgame.gameutil;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.khabu.cardgame.model.game.Round;
+import com.khabu.cardgame.model.game.action.Gamestate;
 import com.khabu.cardgame.model.game.effect.Effect;
 import com.khabu.cardgame.util.IllegalMoveException;
 import com.khabu.cardgame.util.JsonConverter;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 public class GameEffectHandler {
     public static String handleActivateEffect(HashMap<String, Object> jsonMap, Round round) {
@@ -18,5 +21,34 @@ public class GameEffectHandler {
         }
 
         return JsonConverter.createJsonString(new ObjectMapper(), new HashMap<>(), "ACTIVATE_EFFECT");
+    }
+
+    public static String handleFinishEffect(HashMap<String, Object> jsonMap, Round round) {
+        int currentPlayerId = Integer.parseInt((String) jsonMap.get("currentPlayerId"));
+        if (round.getTurn().getCurrentPlayer() != round.getPlayerById(currentPlayerId)) {
+            return "fail";
+        }
+        if (jsonMap.get("swap").equals("true")) {
+            exchangeCardsAfterChecking(jsonMap, round);
+        }
+        // Create response
+        List<String> keys = Arrays.asList("type", "swap");
+        List<String> values = Arrays.asList("FINISH_EFFECT", (String) jsonMap.get("swap"));
+
+        return JsonConverter.createJsonString(new ObjectMapper(), new HashMap<>(), keys, values);
+    }
+
+
+
+    private static void exchangeCardsAfterChecking(HashMap<String, Object> jsonMap, Round round) {
+        try {
+            int currentPlayerId = Integer.parseInt((String) jsonMap.get("currentPlayerId"));
+            round.performEffect(round.getPlayerById(currentPlayerId), 0, Effect.EXCHANGE_AFTER_CHECKS);
+
+            // Clear variables set to handle effect in effectPerformer
+            round.clearTemps();
+        } catch (IllegalMoveException ime) {
+            ime.printStackTrace();
+        }
     }
 }
