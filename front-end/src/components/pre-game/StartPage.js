46b21591c9654c3348d0400916b06f78dc3f1767
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-08-13 13:33:34 +0200

    Change styling of front page

--- front-end/src/components/pre-game/StartPage.js
+++ front-end/src/components/pre-game/StartPage.js
@@ -4,18 +4,17 @@ import KhabuLogo from '../../assets/images/khabu-logo.png';
 
 export default () => {
   return (
-    <div className={'ui centered grid'}>
-      <div className={'four column centered row'}>
+    <div className={'ui grid'}>
+      <div className={'centered row khabu-logo'}>
         <img
           src={KhabuLogo}
           alt={'Khabu logo'}
-          style={{ width: '40%', height: '60%' }}
+          style={{ width: '40%', height: '100%' }}
         />
       </div>
-      <div className={'four column centered row'}>
-        <h1>Khabu</h1>
-      </div>
       <div className={'two column centered row'}>
+        <h1>Khabu</h1>
+        <br />
         <Link to={'/join'}>
           <button className={'ui teal large button'}>Join</button>
         </Link>
