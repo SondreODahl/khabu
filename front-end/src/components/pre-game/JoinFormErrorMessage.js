7802e9b2ad5e0146fec28ec34384bc4ccf98fb97
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-12-14 23:05:43 +0100

    Refactor error message display

--- front-end/src/components/pre-game/JoinFormErrorMessage.js
+++ front-end/src/components/pre-game/JoinFormErrorMessage.js
@@ -4,18 +4,18 @@ import _ from 'lodash';
 // Displays the error message box with the username join form
 const JoinFormErrorMessage = ({ formError, errors }) => {
   const usernameErrors = errors?.username;
-  const patternError = usernameErrors && usernameErrors.types?.pattern;
-  const maxLengthError = usernameErrors && usernameErrors.types?.maxLength;
-  const minLengthError = usernameErrors && usernameErrors.types?.minLength;
-  const requiredError = usernameErrors && usernameErrors.types?.required;
   if (formError || usernameErrors) {
+    const maxLengthError = usernameErrors && usernameErrors.types?.maxLength;
+    const minLengthError = usernameErrors && usernameErrors.types?.minLength;
+    const patternError = usernameErrors && usernameErrors.types?.pattern;
+    const requiredError = usernameErrors && usernameErrors.types?.required;
     return (
       <div className={'ui error message compact'}>
         <ul className={'list'}>
-          {patternError && <li>Username must be only letters and numbers</li>}
-          {maxLengthError && <li>Username must be maximum 20 characters</li>}
-          {minLengthError && <li>Username must be minimum 5 characters</li>}
-          {requiredError && <li>Username must be minimum 5 characters</li>}
+          {maxLengthError && <li>{maxLengthError}</li>}
+          {minLengthError && <li>{minLengthError}</li>}
+          {patternError && <li>{patternError}</li>}
+          {requiredError && <li>{requiredError}</li>}
           {formError && <li>{formError}</li>}
         </ul>
       </div>
