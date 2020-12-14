593ec17ced98b993a9b6221bca31c5f18c28e40d
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-12-14 18:06:00 +0100

    Alphabetically sort constants

--- front-end/src/constants/gameMoves.js
+++ front-end/src/constants/gameMoves.js
@@ -1,6 +1,6 @@
-export const PUT_MOVE = 'PUT_MOVE';
-export const SWAP_MOVE = 'SWAP_MOVE';
-export const DRAW_MOVE = 'DRAW_MOVE';
 export const DISCARD_MOVE = 'DISCARD_MOVE';
-export const TRANSFER_MOVE = 'TRANSFER_MOVE';
+export const DRAW_MOVE = 'DRAW_MOVE';
+export const PUT_MOVE = 'PUT_MOVE';
 export const REVEAL_MOVE = 'REVEAL_MOVE';
+export const SWAP_MOVE = 'SWAP_MOVE';
+export const TRANSFER_MOVE = 'TRANSFER_MOVE';
--- front-end/src/constants/gameStates.js
+++ front-end/src/constants/gameStates.js
@@ -1,9 +1,9 @@
+export const CARD_DRAWN = 'CARD_DRAWN';
+export const DISCARD = 'DISCARD';
 export const DRAW = 'DRAW';
 export const FIRST_TURN = 'FIRST_TURN';
-export const CARD_DRAWN = 'CARD_DRAWN';
+export const FRENZY = 'FRENZY';
 export const PUT = 'PUT';
 export const PUT_FAIL = 'PUT_FAIL';
-export const DISCARD = 'DISCARD';
-export const FRENZY = 'FRENZY';
 export const TRANSFER = 'TRANSFER';
 export const USING_EFFECT = 'USING_EFFECT';
