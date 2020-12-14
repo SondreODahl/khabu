da2780f75b1cc8dcb98239de98622a55bcb8c8af
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-12-14 22:42:54 +0100

    Simplify form using thunk for submit

--- front-end/src/components/pre-game/JoinGamePage.js
+++ front-end/src/components/pre-game/JoinGamePage.js
@@ -2,45 +2,19 @@ import React, { useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { formError, formValid } from '../../actions';
+import { formError, formValid, submitForm } from '../../actions';
 import { axiosConfig } from '../../api/axiosConfig';
 import JoinForm from './JoinForm';
 import { RESET_FORM } from '../../actions/types';
 import { selectFormAttributes } from '../../selectors';
 
+// Page for joining a game with a username
 const JoinGamePage = (props) => {
-  const history = useHistory();
   const dispatch = useDispatch();
-  const { formData, error, submitted, validForm } = useSelector(
-    selectFormAttributes
-  );
+  const { error } = useSelector(selectFormAttributes);
 
   useEffect(() => {
-    if (submitted) {
-      const post = async (url, data) => {
-        console.log('Run now');
-        try {
-          const response = await axiosConfig.post(url, data);
-          console.log(response.data);
-          if (response.data.status.statusCodeValue === 201)
-            dispatch(formValid(response.data));
-          else dispatch(formError('Username taken')); // TODO: Message for full game
-        } catch (err) {
-          dispatch(formError(err.message));
-        }
-      };
-      post('/api/player', { username: formData });
-    }
-  }, [formData, submitted]);
-
-  useEffect(() => {
-    if (validForm) {
-      history.push('/game');
-    }
-  }, [validForm]);
-
-  useEffect(() => {
-    return () => dispatch({ type: RESET_FORM });
+    dispatch({ type: RESET_FORM });
   }, []);
 
   return (
