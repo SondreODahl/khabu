a45ceeb83c3dc97b9f26b2bcd89c1b6754b00223
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-12-14 23:20:56 +0100

    Delete testing

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import useClient from '../api/useSTOMPClient';
-import Testing from './testing/Testing';
 import JoinGamePage from './pre-game/JoinGamePage';
 import StartPage from './pre-game/StartPage';
 import GamePage from './in-game/pages/GamePage';
@@ -16,7 +15,6 @@ const App = () => {
       style={{ marginRight: '20%', marginLeft: '20%', height: '100vh' }}
     >
       <Route path={'/'} exact component={StartPage} />
-      <Route path={'/test'} component={Testing} />
       <Route path={'/join'} component={JoinGamePage} />
       <Route path={'/game'} component={GamePage} />
       <Route path={'/rules'} component={RulesPage} />
