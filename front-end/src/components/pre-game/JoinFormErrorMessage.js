b728d2807e0aa945e8176c67388b0e852ed38367
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-12-14 22:53:34 +0100

    Transform if statements to constants in JoinFormErrorMessage

--- front-end/src/components/pre-game/JoinFormErrorMessage.js
+++ front-end/src/components/pre-game/JoinFormErrorMessage.js
@@ -4,22 +4,18 @@ import _ from 'lodash';
 // Displays the error message box with the username join form
 const JoinFormErrorMessage = ({ formError, errors }) => {
   const usernameErrors = errors?.username;
+  const patternError = usernameErrors && usernameErrors.types?.pattern;
+  const maxLengthError = usernameErrors && usernameErrors.types?.maxLength;
+  const minLengthError = usernameErrors && usernameErrors.types?.minLength;
+  const requiredError = usernameErrors && usernameErrors.types?.required;
   if (formError || usernameErrors) {
     return (
       <div className={'ui error message compact'}>
         <ul className={'list'}>
-          {usernameErrors && usernameErrors.types?.pattern && (
-            <li>Username must be only letters and numbers</li>
-          )}
-          {usernameErrors && usernameErrors.types?.maxLength && (
-            <li>Username must be maximum 20 characters</li>
-          )}
-          {usernameErrors && usernameErrors.types?.minLength && (
-            <li>Username must be minimum 5 characters</li>
-          )}
-          {usernameErrors && usernameErrors.types?.required && (
-            <li>Username must be minimum 5 characters</li>
-          )}
+          {patternError && <li>Username must be only letters and numbers</li>}
+          {maxLengthError && <li>Username must be maximum 20 characters</li>}
+          {minLengthError && <li>Username must be minimum 5 characters</li>}
+          {requiredError && <li>Username must be minimum 5 characters</li>}
           {formError && <li>{formError}</li>}
         </ul>
       </div>
