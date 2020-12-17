e59a9f85a74802870ebd92d61dead5c6d7410cef
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-12-17 16:31:40 +0100

    Remove semantic ui from CardHand

--- front-end/src/components/in-game/cards/CardHand/CardHand.js
+++ front-end/src/components/in-game/cards/CardHand/CardHand.js
@@ -4,22 +4,25 @@ import { selectCardHand } from '../../../../selectors';
 import './CardHand.css';
 import CardWrapper from '../Card/CardWrapper';
 
+// The cardhand for both the player and the opponent.
 const CardHand = (props) => {
   const cards = useSelector((state) => selectCardHand(state, props));
-  const renderCards = () => {
-    return cards.map((id) => {
-      if (id === null) return <div className={'column'}>{null}</div>;
-      return (
-        <CardWrapper
-          playerId={props.playerId}
-          yourId={props.yourId}
-          id={id}
-          key={id}
-        />
-      );
-    });
+  const mapFunction = (id) => {
+    if (id === null) return <div className={'empty-card'}>{null}</div>;
+    return (
+      <CardWrapper
+        playerId={props.playerId}
+        yourId={props.yourId}
+        id={id}
+        key={id}
+      />
+    );
   };
-  return <div className={'ui four column grid card-hand'}>{renderCards()}</div>;
+  const renderCards = () => {
+    if (props.playerId === props.yourId) return cards.map(mapFunction);
+    return cards.reverse().map(mapFunction); // Cards should be in reverse order for the other player
+  };
+  return <div className={'card-hand'}>{renderCards()}</div>;
 };
 
 export default CardHand;
