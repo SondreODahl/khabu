23a6722d1ab0880159075eaa1034d2f70f97effa
Thor-Herman van Eggelen <44784559+Thor-Herman@users.noreply.github.com>
2020-12-19 20:36:43 +0100

    Change card hand appearance to single row (#58)
    * Change card hands to flow on one line and adapt their size
    
    * Scale cards and maintian aspect ratio
    
    * Move buttons to deck-area. Style GameInterface
    
    * Remove temporaryCard div

--- front-end/src/components/in-game/game-page/cards/temporary-card/TemporaryCard.js
+++ front-end/src/components/in-game/game-page/cards/temporary-card/TemporaryCard.js
@@ -4,17 +4,13 @@ import { selectCard } from '../../../../../selectors';
 import { useSelector } from 'react-redux';
 
 // The card shown when drawing from the deck. Is undefined/null when there is no card
-// in the drawn state. 
+// in the drawn state.
 const TemporaryCard = () => {
   const cardId = useSelector((state) => state.cards.temporaryCard);
   if (cardId === undefined || cardId === null) {
     return null; // Not currently drawn.
   }
-  return (
-    <div className={'temporary-card'}>
-      <Card selector={selectCard} onClick={null} id={cardId} />
-    </div>
-  );
+  return <Card selector={selectCard} onClick={null} id={cardId} />;
 };
 
 export default TemporaryCard;
